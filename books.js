9b2fd71b8737d7ba46d125652c5590ed32d76851
Mohamed Maged <abomaged9990@gmail.com>
2025-04-11 18:51:39 +0200

    lab1

---
+++ books.js
@@ -0,0 +1,74 @@
+const books = [
+    {
+        ISBN: '978-0061120084',
+        name: 'To Kill a Mockingbird',
+        category: 'Classic',
+        image: 'https://m.media-amazon.com/images/I/51IXWZzlgSL._SY291_.jpg',
+        pages: 324,
+        author: 'Harper Lee',
+        price: 75
+    },
+    {
+        ISBN: '978-0451524935',
+        name: '1984',
+        category: 'Dystopian',
+        image: 'https://m.media-amazon.com/images/I/41DoIQLHzjL._SY291_.jpg',
+        pages: 28,
+        author: 'George Orwell',
+        price: 65
+    },
+    {
+        ISBN: '978-0743273565',
+        name: 'The Great Gatsby',
+        category: 'Classic',
+        image: 'https://m.media-amazon.com/images/I/41iers+HLSL._SY291_.jpg',
+        pages: 8,
+        author: 'F. Scott Fitzgerald',
+        price: 55
+    },
+    {
+        ISBN: '978-0062315007',
+        name: 'The Alchemist',
+        category: 'Fantasy',
+        image: 'https://m.media-amazon.com/images/I/51Z0nLAfLmL._SY291_.jpg',
+        pages: 208,
+        author: 'Paulo Coelho',
+        price: 60
+    },
+    {
+        ISBN: '978-0307474278',
+        name: 'The Da Vinci Code',
+        category: 'Mystery',
+        image: 'https://m.media-amazon.com/images/I/51Z4nLfLmL._SY291_.jpg',
+        pages: 489,
+        author: 'Dan Brown',
+        price: 80
+    },
+    {
+        ISBN: '978-0545010221',
+        name: 'Harry Potter and the Sorcerer\'s Stone',
+        category: 'Fantasy',
+        image: 'https://m.media-amazon.com/images/I/51HSkTKlauL._SY291_.jpg',
+        pages: 9,
+        author: 'J.K. Rowling',
+        price: 70
+    },
+    {
+        ISBN: '978-0743477109',
+        name: 'The Catcher in the Rye',
+        category: 'Classic',
+        image: 'https://m.media-amazon.com/images/I/518dVCGFBhL._SY291_.jpg',
+        pages: 45,
+        author: 'J.D. Salinger',
+        price: 50
+    },
+    {
+        ISBN: '978-0060850524',
+        name: 'Brave New World',
+        category: 'Dystopian',
+        image: 'https://m.media-amazon.com/images/I/41-n-3hZMeL._SY291_.jpg',
+        pages: 288,
+        author: 'Aldous Huxley',
+        price: 65
+    }
+];
